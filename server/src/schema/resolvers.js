46a7ee67397193c8e6c6cbe4ae0e93f545c09007
jfavlam <jfavlam@pm.me>
2022-04-11 08:29:27 +0200

    feat: Add mocks to resolve main types queries

--- server/src/schema/resolvers.js
+++ server/src/schema/resolvers.js
@@ -1,6 +1,11 @@
 // https://www.apollographql.com/docs/apollo-server/data/resolvers
 
 import { GraphQLScalarType } from "graphql";
+// Mock data
+import { equivalents } from "../data/equivalents.js";
+import { categories } from "../data/categories.js";
+import { subdomains } from "../data/subdomains.js";
+import { domains } from "../data/domains.js";
 // Date scalar
 const dateDefinition = {
   name: "Date",
@@ -27,7 +32,28 @@ const dateDefinition = {
 };
 
 const dateType = new GraphQLScalarType(dateDefinition);
+
 // Resolvers
 export const resolvers = {
+  Query: {
+    equivalents: () => equivalents,
+    categories: () => categories,
+    subdomains: () => subdomains,
+    domains: () => domains,
+    equivalentById(parent, args, context, info) {
+      return equivalents.find(
+        (equivalent) => equivalent.id === Number(args.id)
+      );
+    },
+    categoryById(parent, args, context, info) {
+      return categories.find((category) => category.id === Number(args.id));
+    },
+    subdomainById(parent, args, context, info) {
+      return subdomains.find((subdomain) => subdomain.id === Number(args.id));
+    },
+    domainById(parent, args, context, info) {
+      return domains.find((domain) => domain.id === Number(args.id));
+    },
+  },
   Date: dateType,
 };
