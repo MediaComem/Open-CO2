43f7974cde59bc18e8e6732440f870d6c9ff2310
jfavlam <jfavlam@pm.me>
2022-04-29 09:16:55 +0200

    feat: Add function to generate hash from string

--- server/src/helpers/utils.js
+++ server/src/helpers/utils.js
@@ -39,3 +39,16 @@ export function getAverageFromArray(array, precision = 3) {
   const average = array.reduce((a, b) => a + b, 0) / array.length;
   return average.toFixed(precision);
 }
+
+export function hashString(string, asString, seed) {
+  let hval = seed === undefined ? 0x811c9dc5 : seed;
+
+  for (let i = 0, l = string.length; i < l; i++) {
+    hval ^= string.charCodeAt(i);
+    hval +=
+      (hval << 1) + (hval << 4) + (hval << 7) + (hval << 8) + (hval << 24);
+  }
+  if (asString) return ("0000000" + (hval >>> 0).toString(16)).substr(-8); // Convert to 8 digit hex string
+
+  return hval >>> 0;
+}
