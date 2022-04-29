340da12e62ff7ae27a4189a60bfb3cf3d0423079
jfavlam <jfavlam@pm.me>
2022-04-29 11:23:06 +0200

    feat[DB]: Add DB connector using mongoose

---
+++ server/src/config/database.js
@@ -0,0 +1,45 @@
+import { DEFAULT_MONGO_URI } from "./default.js";
+import mongoose from "mongoose";
+import logger from "./logger.js";
+
+// Mongoose options
+const options = {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+};
+
+// Init DB
+export const initDatabase = () => {
+  mongoose.connection.once("open", function () {
+    logger.info("MongoDB event open");
+    logger.debug("MongoDB connected [%s]", process.env.MONGO_URI);
+
+    // Events
+    mongoose.connection.on("connected", function () {
+      logger.info("MongoDB event connected");
+    });
+
+    mongoose.connection.on("disconnected", function () {
+      logger.warn("MongoDB event disconnected");
+    });
+
+    mongoose.connection.on("reconnected", function () {
+      logger.info("MongoDB event reconnected");
+    });
+
+    mongoose.connection.on("error", function (error) {
+      logger.error("MongoDB event error: " + error);
+    });
+  });
+
+  return mongoose.connect(
+    process.env.MONGO_URI || DEFAULT_MONGO_URI,
+    options,
+    function (error) {
+      if (error) {
+        logger.error("MongoDB connection error: " + error);
+        process.exit(1);
+      }
+    }
+  );
+};
--- server/src/config/default.js
+++ server/src/config/default.js
@@ -1,2 +1,3 @@
 export const DEFAULT_LOG_LEVEL = "info";
 export const DEFAULT_LOG_DIR = "logs/";
+export const DEFAULT_MONGO_URI = "mongodb://localhost:27017/open-co2";
