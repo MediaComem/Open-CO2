a4321b6e827bcf4a9612e7e47488c7c06e724bce
jfavlam <jfavlam@pm.me>
2022-05-04 17:30:00 +0200

    refactor(server): Move all server source

--- server/src/config/default.js
+++
@@ -1,7 +0,0 @@
-export const DEFAULT_LOG_LEVEL = "info";
-export const DEFAULT_LOG_DIR = "logs/";
-export const DEFAULT_PORT = 4000;
-export const DEFAULT_ENDPOINT = "/graphql";
-export const DEFAULT_MONGO_URI = "mongodb://database:27017/open-co2";
-export const DEFAULT_APOLLO_INTROSPECTION = true;
-export const DEFAULT_APOLLO_PLAYGROUND = true;
