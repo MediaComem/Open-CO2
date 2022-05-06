c971696e47fad05b78b245bff715f60beaa0cd47
jfavlam <jfavlam@pm.me>
2022-05-06 12:22:28 +0200

    fix: Update resolver queries name according to Schema

--- server/app/graphql/resolvers.js
+++ server/app/graphql/resolvers.js
@@ -11,7 +11,7 @@ const pkg = JSON.parse(
 // A resolver can optionally accept four positional arguments: (parent, args, context, info)
 export const resolvers = {
   Query: {
-    async getApiInfos() {
+    async infos() {
       try {
         const unitsCount = await Unit.countDocuments();
         const categoriesCount = await Category.countDocuments();
@@ -42,7 +42,7 @@ export const resolvers = {
         logger.error(error);
       }
     },
-    async getCategoryByName(parent, args) {
+    async categoryByName(parent, args) {
       try {
         return await Category.findOne({ name: args.name });
       } catch (error) {
